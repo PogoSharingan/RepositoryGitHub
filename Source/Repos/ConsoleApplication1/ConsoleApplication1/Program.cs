9ffe0674409438dba9a1fbc2e50b2d6691268e4e
Marcin Pogorzelski <marcin.pogorzelski@wp.pl>
2017-02-09 12:44:40 +0100

    add  all enter values to check the task

---
+++ Source/Repos/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter value a");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value b");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value c");
+            int c = int.Parse(Console.ReadLine());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
